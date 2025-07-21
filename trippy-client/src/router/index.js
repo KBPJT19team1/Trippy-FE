55f8c28e69ed2c5ec6d10de000ce02f27fa43630
Jaejeong Lee <leejae938@gmail.com>
2025-07-21 17:37:41 +0900

    [feat] 오늘의 환율 리스트 뷰 1차
    [feat] 오늘의 환율 리스트 뷰 1차

--- trippy-client/src/router/index.js
+++ trippy-client/src/router/index.js
@@ -1,30 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
-import PaymentView from '@/views/PaymentView.vue';
-import TravelLogsView from '@/views/TravelLogsView.vue';
-import MenuView from '@/views/MenuView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import PaymentView from "@/views/PaymentView.vue";
+import TravelLogsView from "@/views/TravelLogsView.vue";
+import MenuView from "@/views/MenuView.vue";
+import ExchangeRateList from "@/components/layouts/exchangeRate/exchangeRateList.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
+      children: [
+        {
+          path: "exchangeRate",
+          component: ExchangeRateList,
+        },
+      ],
     },
     {
-      path: '/payment',
-      name: 'payment',
+      path: "/payment",
+      name: "payment",
       component: PaymentView,
     },
     {
-      path: '/travel-logs',
-      name: 'travel-logs',
+      path: "/travel-logs",
+      name: "travel-logs",
       component: TravelLogsView,
     },
     {
-      path: '/menu',
-      name: 'menu',
+      path: "/menu",
+      name: "menu",
       component: MenuView,
     },
   ],

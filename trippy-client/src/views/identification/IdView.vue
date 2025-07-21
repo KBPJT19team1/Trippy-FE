8b76cffb691b6d08167b6085059a7e17ff1ee1b8
bangbang444 <kim1228com@naver.com>
2025-07-21 17:56:37 +0900

    feat: 신분증 뷰 틀 생성

---
+++ trippy-client/src/views/identification/IdView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full">
+    <TabMenu :tabs="['주민등록', '여권']" v-model:tab="currentTab" />
+    <div v-if="currentTab === '주민등록'" />
+    <div v-if="currentTab === '여권'" />
+  </div>
+</template>
+
+<script setup>
+import TabMenu from "@/components/TabMenu.vue";
+
+import { ref } from "vue";
+const currentTab = ref("주민등록");
+</script>

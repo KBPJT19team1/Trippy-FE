74c1e183729bfa0a5ff9f7a00c33b698f8e59af1
bangbang444 <kim1228com@naver.com>
2025-07-21 17:52:33 +0900

    feat: 신분증뷰 페이지 라우팅

--- trippy-client/src/router/index.js
+++ trippy-client/src/router/index.js
@@ -1,31 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
-import PaymentView from '@/views/PaymentView.vue';
-import TravelLogsView from '@/views/TravelLogsView.vue';
-import MenuView from '@/views/MenuView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import PaymentView from "@/views/PaymentView.vue";
+import TravelLogsView from "@/views/TravelLogsView.vue";
+import MenuView from "@/views/MenuView.vue";
+import IdView from "@/views/identification/IdView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/payment',
-      name: 'payment',
+      path: "/payment",
+      name: "payment",
       component: PaymentView,
     },
     {
-      path: '/travel-logs',
-      name: 'travel-logs',
+      path: "/travel-logs",
+      name: "travel-logs",
       component: TravelLogsView,
     },
     {
-      path: '/menu',
-      name: 'menu',
+      path: "/menu",
+      name: "menu",
       component: MenuView,
+    },
+    {
+      path: "/identification",
+      name: "identification",
+      component: IdView,
     },
   ],
 });

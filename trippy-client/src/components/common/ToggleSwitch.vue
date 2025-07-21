4efed5fb69fe33f2beb77dbbfb87947d02feb895
i-soj-ng <jeong4530@gmail.com>
2025-07-21 16:26:22 +0900

    feat : ToggleSwitch 컴포넌트 추가
    - Custom ToggleSwitch 컴포넌트 추가

---
+++ trippy-client/src/components/common/ToggleSwitch.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const isClicked = ref(false)
+const toggle = () => {
+  isClicked.value = !isClicked.value
+}
+</script>
+
+<template>
+  <div class="flex items-center">
+    <button
+      @click="toggle"
+      :class="[
+        'relative inline-flex h-6 w-14 items-center rounded-full transition-colors duration-300',
+        isClicked ? 'bg-blue-400' : 'bg-gray-300'
+      ]"
+    >
+      <span
+        :class="[
+          'inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-300',
+          isClicked ? 'translate-x-9' : 'translate-x-1'
+        ]"
+      />
+    </button>
+  </div>
+</template>
--- trippy-client/src/views/HomeView.vue
+++ trippy-client/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import ToggleSwitch from '@/components/common/ToggleSwitch.vue';
 </script>
 
 <template>
-  <main>
-    <h1>home</h1>
+  <main class='w-full flex flex-col gap-4'>
+    <div class="flex items-center gap-1">
+      <ToggleSwitch />
+      <span class='caption2 text-gray-400'>모임통장 보기</span>
+    </div>
   </main>
 </template>

b275b969e752a26b426a89d527c98d917820c19a
i-soj-ng <jeong4530@gmail.com>
2025-07-21 20:27:41 +0900

    feat : AccoutCard 컴포넌트 추가
    - 계좌 표시 Card 컴포넌트 추가
    - 내 계좌 데이터 있는/없는 경우별 조건부 렌더링

--- trippy-client/src/views/HomeView.vue
+++ trippy-client/src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <script setup>
+import { Icon } from '@iconify/vue'
 import ToggleSwitch from '@/components/common/ToggleSwitch.vue';
+import AccountCard from '@/components/home/AccountCard.vue';
 </script>
 
 <template>
-  <main class='w-full flex flex-col gap-4'>
-    <div class="flex items-center gap-1">
-      <ToggleSwitch />
-      <span class='caption2 text-gray-400'>모임통장 보기</span>
+  <main class='w-full flex flex-col gap-8'>
+    <div class='flex flex-col gap-4'>
+      <div class="flex items-center gap-1">
+        <ToggleSwitch />
+        <span class='caption2 text-gray-400'>모임통장 보기</span>
+      </div>
+      <AccountCard />
     </div>
   </main>
 </template>

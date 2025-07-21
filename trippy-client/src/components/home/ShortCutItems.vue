5d5a8bdb02c0ae59f5ab2392341fac7874e57fc0
i-soj-ng <jeong4530@gmail.com>
2025-07-21 20:53:37 +0900

    feat : 바로가기 아이콘 기능 연결
    - 아이콘별 바로가기 링크 연결

--- trippy-client/src/components/home/ShortCutItems.vue
+++ trippy-client/src/components/home/ShortCutItems.vue
@@ -4,19 +4,23 @@ import { Icon } from '@iconify/vue'
 const shortcutItems = [
   {
     icon: 'material-symbols:currency-exchange-rounded',
-    title: '환전하기'
+    title: '환전하기',
+    to: '/exchange'
   },
   {
     icon: 'material-symbols:id-card-rounded',
-    title: '신분증/여권\n확인하기'
+    title: '신분증/여권\n확인하기',
+    to: '/id-cards'
   },
   {
     icon: 'material-symbols:airplane-ticket',
-    title: '항공권/바우처\n확인하기'
+    title: '항공권/바우처\n확인하기',
+    to: '/tickets'
   },
   {
     icon: 'material-symbols:g-translate',
-    title: '번역하기'
+    title: '번역하기',
+    to: '/translation'
   },
 ]
 </script>
@@ -25,9 +29,13 @@ const shortcutItems = [
   <div
     v-for='(item, index) in shortcutItems'
     :key='index'
-    class='w-full h-24 bg-white rounded-xl flex flex-col items-center justify-center gap-1'
+    class='w-full'
   >
-    <Icon :icon='item.icon' class='w-8 h-8 text-blue-300 hover:text-blue-400' />
-    <p class='caption3 text-gray-600 text-center whitespace-pre-line'>{{ item.title }}</p>
+    <RouterLink :to='item.to'>
+      <div class='w-full h-24 bg-white rounded-xl flex flex-col items-center justify-center gap-1'>
+        <Icon :icon='item.icon' class='w-8 h-8 text-blue-300 hover:text-blue-400' />
+        <p class='caption3 text-gray-600 text-center whitespace-pre-line'>{{ item.title }}</p>
+      </div>
+    </RouterLink>
   </div>
 </template>

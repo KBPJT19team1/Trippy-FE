06444374e3fd2e3d25d3bde2b53d39ccc8b63a58
kang-yesung <rkddptjd5228@gmail.com>
2025-07-23 19:14:24 +0900

    chore 비밀번호 설정 컴포넌트 수정
    - props로 정보 전달 할 할 수 있게 수정
    - 입력 창 클릭시 테두리 색상 변경

--- trippy-client/src/components/group-account/AgreementAccountPassword.vue
+++ trippy-client/src/components/group-account/AgreementAccountPassword.vue
@@ -1,7 +1,11 @@
 <script setup>
-import { defineModel, ref } from 'vue';
+import { defineModel, defineProps, ref } from 'vue';
 
 const pin = defineModel();
+
+const props = defineProps({
+  title: String,
+})
 
 const hiddenInput = ref(null)
 
@@ -15,9 +19,10 @@ const focusInput = () => {
 </script>
 
 <template>
-  <div @click="focusInput" class="flex items-center
- justify-between bg-white m-2 w-full h-15 p-4 rounded-xl hover:opacity-90 shadow-custom body2 text-gray-600">
-    <p>통장 비밀번호 만들기</p>
+  <div @click="focusInput"
+    class="flex items-center
+ justify-between bg-white m-2 w-full h-15 p-4 rounded-xl shadow-custom body2 text-gray-600 focus-within:ring-1 ring-blue">
+    <p>{{ props.title }}</p>
     <input type="number" class="sr-only" v-model="pin" @input="onInput" ref="hiddenInput" />
     <div class="flex gap-2">
       <div v-for="i in 4" :key="i" :class="[i <= pin.length ? 'bg-main-gradient' : 'bg-gray-300']"
--- trippy-client/src/views/group-account/GroupAccountAgreementStep3View.vue
+++ trippy-client/src/views/group-account/GroupAccountAgreementStep3View.vue
@@ -2,6 +2,7 @@
 import AgreementAccountPassword from '@/components/group-account/AgreementAccountPassword.vue';
 import { ref } from 'vue'
 
+
 const pin = ref('');
 const pinChexk = ref("")
 
@@ -9,15 +10,17 @@ const pinChexk = ref("")
 </script>
 
 <template>
-  <div class="bg-main-gradient mt-10 w-full h-40 p-4 rounded-xl hover:opacity-90">
+  <div class="bg-main-gradient mt-10 w-full h-40 p-4 rounded-xl">
     <div class="flex gap-2 flex-col items-center justify-center h-full text-white">
       <p class="title2">Trippy</p>
       <p class="title1">모임통장</p>
     </div>
   </div>
 
-  <AgreementAccountPassword v-model="pin" />
-  <AgreementAccountPassword v-model="pinChexk" />
+  <div class="w-full flex flex-col items-center mt-5">
+    <AgreementAccountPassword v-model="pin" :title="'통장 비밀번호 설정'" />
+    <AgreementAccountPassword v-model="pinChexk" :title="'통장 비밀번호 확인'" />
+  </div>
 
 
 

26dd1ed6e619dd79d1f5b94942777fa4e7e01f74
kang-yesung <rkddptjd5228@gmail.com>
2025-07-23 18:51:16 +0900

    feat 비밀번호 설정 컴포넌트 생성 및 그림자 속성 추가
    - 비밀번호 입력에 따라 원 색상 변화
    - 그림자 속성 추가

---
+++ trippy-client/src/components/group-account/AgreementAccountPassword.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineModel, ref } from 'vue';
+
+const pin = defineModel();
+
+const hiddenInput = ref(null)
+
+const onInput = (event) => {
+  const value = event.target.value
+  pin.value = value.slice(0, 4)
+};
+const focusInput = () => {
+  hiddenInput.value?.focus()
+}
+</script>
+
+<template>
+  <div @click="focusInput" class="flex items-center
+ justify-between bg-white m-2 w-full h-15 p-4 rounded-xl hover:opacity-90 shadow-custom body2 text-gray-600">
+    <p>통장 비밀번호 만들기</p>
+    <input type="number" class="sr-only" v-model="pin" @input="onInput" ref="hiddenInput" />
+    <div class="flex gap-2">
+      <div v-for="i in 4" :key="i" :class="[i <= pin.length ? 'bg-main-gradient' : 'bg-gray-300']"
+        class="bg-gray-300 w-4 h-4 rounded-[50%]"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ trippy-client/src/views/group-account/GroupAccountAgreementStep3View.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AgreementAccountPassword from '@/components/group-account/AgreementAccountPassword.vue';
+import { ref } from 'vue'
+
+const pin = ref('');
+const pinChexk = ref("")
+
+
+</script>
+
+<template>
+  <div class="bg-main-gradient mt-10 w-full h-40 p-4 rounded-xl hover:opacity-90">
+    <div class="flex gap-2 flex-col items-center justify-center h-full text-white">
+      <p class="title2">Trippy</p>
+      <p class="title1">모임통장</p>
+    </div>
+  </div>
+
+  <AgreementAccountPassword v-model="pin" />
+  <AgreementAccountPassword v-model="pinChexk" />
+
+
+
+</template>
+
+<style scoped></style>

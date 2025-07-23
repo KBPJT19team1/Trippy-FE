b1968761ce41baa1470a5228036101094cfbd72c
bangbang444 <kim1228com@naver.com>
2025-07-23 16:09:46 +0900

    chore: prittier 적용

---
+++ trippy-client/src/components/common/TabMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="flex w-full border-b bg-white">
+      <button
+        v-for="tabItem in tabs"
+        :key="tabItem"
+        @click="$emit('update:tab', tabItem)"
+        :class="[
+          'py-2 text-base font-medium transition-colors flex-1',
+          tab === tabItem ? 'text-blue-500 border-b-2 border-blue-500' : 'text-gray-400',
+        ]"
+      >
+        {{ tabItem }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  tabs: { type: Array, required: true },
+  tab: { type: String, required: true },
+});
+
+defineEmits(["update:tab"]);
+</script>

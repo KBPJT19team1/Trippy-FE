e1441d9e035e683f6379206e4c8b1d0fea9fcd30
bangbang444 <kim1228com@naver.com>
2025-07-23 14:41:47 +0900

    feat: 신분증 조회 뷰
    등록한 신분증을 조회하는 뷰입니다.
    구현한 것
    - 그라데이션 css로  적용
    1. 상세보기 전
    - 상단 주의사항 바
    - mock 신분증 이미지 삽입
    - mock QR 및 남은 시간 삽입
    - 이름 삽입
    2. 상세보기 후
    - 주민등록번호, 주소, 발급일자 표시
    - 토글 기능 추가(주민등록번호 뒷자리 * 표시 여부)
    - 버튼부분 'QR 정보 표시'로 변경

--- trippy-client/src/views/identification/IdView.vue
+++ trippy-client/src/views/identification/IdView.vue
@@ -4,7 +4,11 @@
 
     <!-- 색깔,  임시로 넣은거 피그마 참고하고 다시 넣기 -->
     <div v-if="currentTab === '주민등록'">
-      <div class="w-full h-[34rem] rounded-xl shadow-md bg-white flex flex-col">
+      <!-- 신분증 등록 안 된 경우 -->
+      <div
+        v-if="!isRegistered"
+        class="w-full h-[32rem] rounded-xl shadow-md bg-white flex flex-col"
+      >
         <div class="flex-1 flex flex-col items-center justify-center">
           <img :src="Idcard" class="w-[15rem] h-auto mb-2" />
           <!-- 안내 문구 -->
@@ -16,15 +20,97 @@
 
         <!-- 등록 버튼 -->
         <button
-          class="w-full py-1.5 rounded-b-lg shadow-md bg-gradient-to-r from-blue-500 to-blue-600 text-white font-bold text-center mt-auto"
+          class="card-footer-button w-full py-3 rounded-b-lg shadow-md text-white font-bold text-center mt-auto"
         >
           주민등록증 등록
+        </button>
+      </div>
+
+      <!-- 신분증 등록된 경우 -->
+      <div
+        v-else
+        class="w-full h-[32rem] rounded-xl shadow-md flex flex-col"
+        :class="{ 'bg-gray-100': showDetail, 'bg-white': !showDetail }"
+      >
+        <!-- 안내 문구 -->
+        <!-- 글씨 크기가 더 작아야 해서 0.62rem으로 일단 넣음 -->
+        <div
+          class="card-header-gradient w-full text-white flex flex-col text-[0.62rem] py-1 rounded-t-xl whitespace-nowrap"
+        >
+          주민등록증 이미지 위변조 및 부정 사용 금지(위반 시 3년 이하의 징역 또는 3천만원 벌금)
+        </div>
+
+        <!-- 디폴트 프로필 -->
+        <img :src="DefaultProfile" />
+
+        <div class="mt-4 flex w-full items-center-justify-start px-4">
+          <div class="flex flex-col">
+            <!-- 안내 문구 -->
+            <p class="text-gray-500 text-xxs">모바일 신분증 확인 서비스</p>
+            <!-- 이름 -->
+            <h2 class="mt-1 text-xl font-semibold">홍길동</h2>
+
+            <div v-if="showDetail">
+              <div class="mt-1 text-xl font-semibold">
+                <h2>{{ maskedId }}</h2>
+              </div>
+              <div class="mt-9">
+                <p>서울 특별시 광진구 능동로 195-16</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- 내용 -->
+          <div class="flex flex-col item-center ml-auto">
+            <!-- QR -->
+            <div v-if="!showDetail">
+              <div class="w-32 h-32 mb-3 bg-white-300 rounded-md flex item-center justify-center">
+                <img :src="DefaultQr" alt="QR Code" class="max-w-full max-h-full" />
+              </div>
+              <!-- 진행바 -->
+              <div class="w-full h-1 bg-gray-200 rounded-full mt-1">
+                <div class="h-1 bg-blue-500 rounded-full" style="width: 70%"></div>
+              </div>
+              <!-- 남은 시간 -->
+              <p class="text-gray-400 text-xxs">남은 시간: 8초</p>
+            </div>
+
+            <div v-else>
+              <!-- 토글 -->
+              <label class="inline-flex items-center cursor-pointer">
+                <input type="checkbox" v-model="toggleOn" class="sr-only" />
+
+                <div class="w-14 h-6 bg-black rounded-full relative transition-colors duration-300">
+                  <div
+                    class="w-5 h-5 bg-white rounded-full absolute top-0.5 left-0.5 transition-transform duration-300"
+                    :class="{ 'translate-x-8': toggleOn }"
+                  ></div>
+                </div>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <!-- 법적 효력 안내 -->
+        <div class="p-4 mt-auto">
+          <div class="flex items-center justify-between">
+            <!-- TODO: 링크 걸어두기 -->
+            <p class="text-gray-500 text-xxs">법적 효력 안내 ></p>
+            <p class="text-sm font-semibold">2019.12.13.</p>
+          </div>
+        </div>
+        <!-- 상세정보 표시버튼 -->
+        <button
+          class="card-footer-button w-full py-3 rounded-b-lg shadow-md bg-gradient-to-r from-blue-500 to-blue-600 text-white font-bold text-center"
+          @click="showDetail = !showDetail"
+        >
+          {{ showDetail ? "QR 정보 표시" : "상세정보 표시" }}
         </button>
       </div>
     </div>
 
     <div v-if="currentTab === '여권'">
-      <h1>hi</h1>
+      <h1>여권</h1>
     </div>
   </div>
 </template>
@@ -32,8 +118,37 @@
 <script setup>
 import Idcard from "@/assets/Idcard.png";
 import TabMenu from "@/components/TabMenu.vue";
+import DefaultProfile from "@/assets/default_profile.png";
+import DefaultQr from "@/assets/default_qr.png";
+import { computed, ref } from "vue";
 
-import { ref } from "vue";
-
+const isRegistered = ref(true); // 임시로 고정 설정
 const currentTab = ref("주민등록");
+const showDetail = ref(false);
+const toggleOn = ref(false);
+
+const rawId = "010123-1234567";
+const maskedId = computed(() => {
+  const [front, back] = rawId.split("-");
+  if (!toggleOn.value) return rawId; // 토글 OFF → 전체 표시
+  const maskedBack = back[0] + "*".repeat(back.length - 1); // 첫 자리만 남기고 마스킹
+  return `${front}-${maskedBack}`;
+});
 </script>
+
+<style scoped>
+.card-header-gradient {
+  background: linear-gradient(90deg, #6ae4ff 0%, #236fff 43.23%, #236fff 58.33%, #a38eff 100%);
+  border-radius: 10px 10px 0 0;
+}
+.card-footer-button {
+  background: linear-gradient(
+    152.02deg,
+    #6ae4ff -45.76%,
+    #236fff 25.9%,
+    #236fff 50.94%,
+    #a38eff 120.01%
+  );
+  border-radius: 0 0 10px 10px;
+}
+</style>

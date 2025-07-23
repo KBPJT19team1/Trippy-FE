c6a83866dba36db170d4fccee72676f48ab3c31f
kang-yesung <rkddptjd5228@gmail.com>
2025-07-23 11:33:37 +0900

    feat 로딩 뷰
    - 로딩 뷰 컴포넌트 분리 완료

--- trippy-client/src/components/common/EmailInput.vue
+++ trippy-client/src/components/common/EmailInput.vue
@@ -5,7 +5,7 @@ const model = defineModel();
 <template>
   <div class="flex justify-center h-12 w-[21.4375rem] ">
     <input v-model="model" type="email" placeholder="이메일을 입력해 주세요"
-      class="w-full px-6 py-4 bg-gray-200 text-break placeholder-gray-500 rounded-[0.625rem] caption1" />
+      class="w-full px-6 py-4 bg-gray-200 text-center text-break placeholder-gray-500 rounded-[0.625rem] caption1" />
   </div>
 </template>
 

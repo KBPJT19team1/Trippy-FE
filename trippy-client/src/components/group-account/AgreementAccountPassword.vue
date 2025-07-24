f9e2344176b0c8d3c2e927ad7d97ce2c984540f7
kang-yesung <rkddptjd5228@gmail.com>
2025-07-24 15:42:47 +0900

    refactor input 스타일 변경
    - number 스타일로 지정시 방향키로 값이 변경 되어 text 스타일로 변경

--- trippy-client/src/components/group-account/AgreementAccountPassword.vue
+++ trippy-client/src/components/group-account/AgreementAccountPassword.vue
@@ -10,8 +10,7 @@ const props = defineProps({
 const hiddenInput = ref(null)
 
 const onInput = (event) => {
-  const value = event.target.value
-  pin.value = value.slice(0, 4)
+  pin.value = event.target.value.replace(/[^0-9]/g, "").slice(0, 4)
 };
 const focusInput = () => {
   hiddenInput.value?.focus()
@@ -23,7 +22,8 @@ const focusInput = () => {
     class="flex items-center
  justify-between bg-white m-2 w-full h-15 p-4 rounded-xl shadow-custom body2 text-gray-600 focus-within:ring-1 ring-blue">
     <p>{{ props.title }}</p>
-    <input type="number" inputmode="numeric" class="sr-only" v-model="pin" @input="onInput" ref="hiddenInput" />
+    <input type="text" inputmode="numeric" pattern="[0-9]*" class="sr-only" v-model="pin" @input="onInput"
+      ref="hiddenInput" />
     <div class="flex gap-2">
       <div v-for="i in 4" :key="i" :class="[i <= pin.length ? 'bg-main-gradient' : 'bg-gray-300']"
         class="bg-gray-300 w-4 h-4 rounded-[50%]"></div>

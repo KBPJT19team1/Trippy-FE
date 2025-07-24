82d88b9dca676dcb56ac0d38b1bffa33d4d06450
hellozo0 <ahju0011@gmail.com>
2025-07-24 15:52:34 +0900

    feat : 내역불러오기 클릭시 로딩 컴포넌트 연결

---
+++ trippy-client/src/components/common/LoadingOverlay.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    default: "로딩 중입니다...", // default 메시지
+  },
+});
+</script>
+
+<template>
+  <div
+    class="absolute inset-0 z-[999] bg-black/40 backdrop-blur-sm flex flex-col items-center justify-center"
+  >
+    <svg class="animate-spin h-16 w-16 mb-4" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="25" cy="25" r="20" stroke="white" stroke-width="6" fill="none" />
+
+      <circle
+        cx="25"
+        cy="25"
+        r="20"
+        stroke="url(#mainGradient)"
+        stroke-width="6"
+        stroke-linecap="round"
+        stroke-dasharray="110"
+        stroke-dashoffset="35"
+        fill="none"
+      />
+
+      <!-- svg에 tailwind 설정 컬러 적용이 안되는 문제로 추가 정의 -->
+      <defs>
+        <linearGradient id="mainGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stop-color="#6AE4FF" />
+          <stop offset="25.9%" stop-color="#236FFF" />
+          <stop offset="50.94%" stop-color="#236FFF" />
+          <stop offset="100%" stop-color="#A38EFF" />
+        </linearGradient>
+      </defs>
+    </svg>
+    <p class="text-white button1">{{ message }}</p>
+  </div>
+</template>
--- trippy-client/src/views/air-ticket/AirTicketView.vue
+++ trippy-client/src/views/air-ticket/AirTicketView.vue
@@ -1,36 +1,45 @@
 <script setup>
 import { useAirTicketStore } from "@/stores/airTicketStore";
 import { storeToRefs } from "pinia";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import dummyTickets from "@/_dummy/airTicket_dummy.json"; //더미 -> 나중에 삭제하기
 import QuickReloadButton from "@/components/common/QuickReloadButton.vue";
+import LoadingOverlay from "@/components/common/LoadingOverlay.vue";
 import EmptyTicket from "@/components/air-ticket/EmptyTicket.vue";
 import TicketList from "@/components/air-ticket/TicketItem.vue";
 
 const store = useAirTicketStore();
 const { tickets } = storeToRefs(store);
+const isLoading = ref(false);
 
 onMounted(() => {
   store.setTickets(dummyTickets);
 });
 
 const onReload = () => {
-  console.log(" Reload 버튼 클릭 ");
+  isLoading.value = true;
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 2000);
 };
 </script>
 
 <template>
   <main class="w-full flex flex-col gap-8">
-    <!-- ✅ 항공권 없을 때 -->
+    <!-- 로딩 블러 오버레이 -->
+    <LoadingOverlay v-if="isLoading" message="예약한 항공권을 불러오고 있습니다." />
+
+    <!-- 항공권 없을 때 -->
     <div v-if="tickets.length === 0" class="mt-[50%] flex justify-center">
       <EmptyTicket />
     </div>
 
-    <!-- ✅ 항공권 있을 때 -->
+    <!-- 항공권 있을 때 -->
     <div v-else>
       <TicketList :tickets="tickets" />
     </div>
 
+    <!-- 하단 퀵 버튼 => ❌애 문제 있음 -->
     <div class="fixed bottom-8 right-5 z-50">
       <QuickReloadButton @reload="onReload" />
     </div>

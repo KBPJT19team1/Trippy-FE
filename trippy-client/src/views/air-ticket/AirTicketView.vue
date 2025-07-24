dd0ad921aa419274632c58c45a5d3afd503d2e80
hellozo0 <ahju0011@gmail.com>
2025-07-24 15:13:05 +0900

    feat : 항공권 있을때 없을때 조건 뷰 처리

--- trippy-client/src/views/air-ticket/AirTicketView.vue
+++ trippy-client/src/views/air-ticket/AirTicketView.vue
@@ -1,5 +1,18 @@
 <script setup>
+import { useAirTicketStore } from "@/stores/airTicketStore";
+import { storeToRefs } from "pinia";
+import { onMounted } from "vue";
+import dummyTickets from "@/_dummy/airTicket_dummy.json"; //더미 -> 나중에 삭제하기
 import QuickReloadButton from "@/components/common/QuickReloadButton.vue";
+import EmptyTicket from "@/components/air-ticket/EmptyTicket.vue";
+import TicketList from "@/components/air-ticket/TicketItem.vue";
+
+const store = useAirTicketStore();
+const { tickets } = storeToRefs(store);
+
+onMounted(() => {
+  store.setTickets(dummyTickets);
+});
 
 const onReload = () => {
   console.log(" Reload 버튼 클릭 ");
@@ -8,7 +21,17 @@ const onReload = () => {
 
 <template>
   <main class="w-full flex flex-col gap-8">
-    <div class="relative min-h-screen">
+    <!-- ✅ 항공권 없을 때 -->
+    <div v-if="tickets.length === 0" class="mt-[50%] flex justify-center">
+      <EmptyTicket />
+    </div>
+
+    <!-- ✅ 항공권 있을 때 -->
+    <div v-else>
+      <TicketList :tickets="tickets" />
+    </div>
+
+    <div class="fixed bottom-8 right-5 z-50">
       <QuickReloadButton @reload="onReload" />
     </div>
   </main>

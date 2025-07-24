ba397f244ca131dc752074b80264e0ee6c7e4101
hellozo0 <ahju0011@gmail.com>
2025-07-24 10:18:31 +0900

    feat : 내역 불러오기 퀵 버튼 컴포넌트 구현

---
+++ trippy-client/src/components/common/QuickReloadButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineEmits } from "vue";
+import { Icon } from "@iconify/vue";
+
+const emits = defineEmits(["reload"]);
+const onClick = () => {
+  emits("reload");
+};
+</script>
+
+<template>
+  <div class="absolute bottom-6 right-4 z-50">
+    <button
+      @click="onClick"
+      class="flex items-center gap-2 px-4 py-3 bg-main-gradient text-white rounded-full shadow-md hover:opacity-90 transition"
+    >
+      <Icon icon="material-symbols:refresh-rounded" class="w-6 h-6" />
+      <span class="button2 text-white">내역 불러오기</span>
+    </button>
+  </div>
+</template>

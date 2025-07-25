b2f394fcdf07c4f0195c6873b0cda36434956eac
hellozo0 <ahju0011@gmail.com>
2025-07-25 13:21:06 +0900

    feat : 뷰 전체에 로딩 적용, 스크롤 막기 로직

--- trippy-client/src/components/common/LoadingOverlay.vue
+++ trippy-client/src/components/common/LoadingOverlay.vue
@@ -1,15 +1,30 @@
 <script setup>
+import { onMounted, onUnmounted } from "vue";
+
 defineProps({
   message: {
     type: String,
     default: "로딩 중입니다...", // default 메시지
   },
 });
+
+onMounted(() => {
+  document.body.style.overflow = "hidden";
+});
+
+onUnmounted(() => {
+  document.body.style.overflow = "";
+});
 </script>
 
 <template>
+  <!-- 위의 상단 탭바는 살려두고 아래에만 로딩 -->
+  <!-- <div
+    class="absolute top-[100px] left-0 right-0 h-[calc(100vh)] z-[999] bg-black/40 backdrop-blur-sm flex flex-col items-center justify-center"
+  > -->
+  <!-- 전체 뷰에 로딩  -->
   <div
-    class="absolute inset-0 z-[999] bg-black/40 backdrop-blur-sm flex flex-col items-center justify-center"
+    class="fixed inset-0 z-[9999] bg-black/40 backdrop-blur-sm flex flex-col items-center justify-center"
   >
     <svg class="animate-spin h-16 w-16 mb-4" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
       <circle cx="25" cy="25" r="20" stroke="white" stroke-width="6" fill="none" />

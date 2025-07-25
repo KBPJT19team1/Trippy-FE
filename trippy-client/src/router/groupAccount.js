af89884a3cb4b4d068727d7122cd07fd6660780f
kang-yesung <rkddptjd5228@gmail.com>
2025-07-25 17:17:29 +0900

    chore veiw이름 수정 및 router 수정

--- trippy-client/src/router/groupAccount.js
+++ trippy-client/src/router/groupAccount.js
@@ -2,27 +2,27 @@ export default [
   {
     path: "group-account",
     name: "group-account-intro",
-    component: () => import("../views/group-account/GroupAccountCreateIntroView.vue"),
+    component: () => import("../views/group-account/CreateIntroView.vue"),
   },
   {
     path: "group-account/step1",
     name: "group-account-step1",
-    component: () => import("../views/group-account/GroupAccountAgreementStep1View.vue"),
+    component: () => import("../views/group-account/AgreementStep1View.vue"),
   },
   {
     path: "group-account/step2",
     name: "group-account-step2",
-    component: () => import("../views/group-account/GroupAccountAgreementStep2View.vue"),
+    component: () => import("../views/group-account/AgreementStep2View.vue"),
   },
   {
     path: "group-account/step3",
     name: "group-account-step3",
-    component: () => import("../views/group-account/GroupAccountAgreementStep3View.vue"),
+    component: () => import("../views/group-account/AgreementStep3View.vue"),
   },
   {
     path: "/group-account/step4",
     name: "group-account-step4",
-    component: () => import("../views/group-account/GroupAccountAgreementStep4View.vue"),
+    component: () => import("../views/group-account/AgreementStep4View.vue"),
   },
   {
     path: "/group-account/create-complete",

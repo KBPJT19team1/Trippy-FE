3787e72dbef16cd5085e8b406fe5c35e52cce473
kang-yesung <rkddptjd5228@gmail.com>
2025-07-26 18:48:56 +0900

    refactor 모임계좌 개설 뷰 전체 버튼 위치 수정

--- trippy-client/src/views/group-account/AgreementStep1View.vue
+++ trippy-client/src/views/group-account/AgreementStep1View.vue
@@ -39,23 +39,40 @@ const toggleItem = (index) => {
 </script>
 
 <template>
-  <div class="con">
-    <TrippyLogo class="w-64 mt-30 m-auto mt-[8.5vh]" />
-    <AgreementCheck @click="toggleAllCheck" :class="[!allChecked ? 'bg-gray-400' : 'bg-main-gradient']" class="mt-14"
-      title="전체 동의" />
+  <div class="flex flex-col h-full overflow-hidden justify-between">
+    <div>
+      <div>
+        <TrippyLogo class="w-64 m-auto mt-[4vh]" />
+        <AgreementCheck
+          @click="toggleAllCheck"
+          :class="[!allChecked ? 'bg-gray-400' : 'bg-main-gradient']"
+          class="mt-14"
+          title="전체 동의"
+        />
+      </div>
+      <AgreementItem
+        v-for="(item, index) in agreementStep1"
+        :key="index"
+        :title="item.title"
+        :visible="checkedItems[index]"
+        @click="() => toggleItem(index)"
+      />
+      <EmailInput class="mt-2" v-model="email" />
+      <div class="mt-5">
+        <AccountNotice
+          class="mt-1 caption3 text-gray-400 text-center"
+          v-for="(item, index) in agreementStep2"
+          :key="index"
+          :title="item.title"
+        />
+      </div>
+    </div>
+    <NextButton
+      title="다음"
+      :disabled="!formValid"
+      @click="router.push({ name: 'group-account-step2' })"
+    />
   </div>
-  <AgreementItem v-for="(item, index) in agreementStep1" :key="index" :title="item.title" :visible="checkedItems[index]"
-    @click="() => toggleItem(index)" />
-  <EmailInput class="mt-2" v-model="email" />
-  <div class="mt-5">
-    <AccountNotice class="mt-1 caption3 text-gray-400 text-center" v-for="(item, index) in agreementStep2" :key="index"
-      :title="item.title" />
-  </div>
-  <NextButton
-    title="다음"
-    :disabled="!formValid"
-    @click="router.push({ name: 'group-account-step2' })"
-  />
 </template>
 
 <style scoped></style>

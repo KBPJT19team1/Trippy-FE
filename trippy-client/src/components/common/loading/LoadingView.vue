1c240ffdaf0ee0c578012896f40e71c897d4b58a
i-soj-ng <jeong4530@gmail.com>
2025-07-28 11:22:24 +0900

    feat : 로딩 뷰 퍼블리싱
    - 1.5초 후 다음 페이지로 넘어가도록 임시 설정

---
+++ trippy-client/src/components/common/loading/LoadingView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps, defineEmits, onMounted } from "vue";
+import OnLoading from "@/components/common/loading/OnLoading.vue";
+
+const props = defineProps({
+  description: String,
+});
+
+const emit = defineEmits(["next"]);
+
+// (임시) 1.5초 후 다음 페이지로 넘어감
+onMounted(() => {
+  setTimeout(() => {
+    emit("next");
+  }, 1500);
+});
+</script>
+
+<template>
+  <div class="flex flex-col h-full items-center justify-center gap-8 pb-2">
+    <OnLoading />
+    <p class="body1">{{ props.description }}</p>
+  </div>
+</template>
--- trippy-client/src/views/personal-accounts/ImportAccountView.vue
+++ trippy-client/src/views/personal-accounts/ImportAccountView.vue
@@ -5,11 +5,14 @@ import FirstStep from "@/components/personal-accounts/import-accounts/FirstStep.
 import SecondStep from "@/components/personal-accounts/import-accounts/SecondStep.vue";
 import ThirdStep from "@/components/personal-accounts/import-accounts/ThirdStep.vue";
 import FourthStep from "@/components/personal-accounts/import-accounts/AgreeToTerms.vue";
+import LoadingView from "@/components/common/loading/LoadingView.vue";
 
-const views = [FirstStep, SecondStep, ThirdStep, FourthStep];
-const currentIndex = ref(3);
+const views = [FirstStep, SecondStep, ThirdStep, FourthStep, LoadingView];
+const currentIndex = ref(4);
 
 const currentView = computed(() => views[currentIndex.value]);
+
+const loadingDescription = "계좌 목록을 불러오고 있습니다.";
 
 function goNext() {
   if (currentIndex.value < views.length - 1) {
@@ -20,6 +23,10 @@ function goNext() {
 
 <template>
   <main class="w-full bg-white h-full">
-    <component :is="currentView" @next="goNext" />
+    <component
+      :is="currentView"
+      @next="goNext"
+      v-bind="currentView === LoadingView ? { description: loadingDescription } : {}"
+    />
   </main>
 </template>

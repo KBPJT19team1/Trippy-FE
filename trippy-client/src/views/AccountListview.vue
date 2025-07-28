fdc04f9eeb7a2b5ae797cb6432488252590bb19c
kang-yesung <rkddptjd5228@gmail.com>
2025-07-28 11:44:57 +0900

    feat 토글 상태에 따라 계좌 추가하기 분기 처리

--- trippy-client/src/views/AccountListview.vue
+++ trippy-client/src/views/AccountListview.vue
@@ -1,6 +1,7 @@
 <script setup>
 import UnifiedAccountCard from "@/components/account/UnifiedAccountCard.vue";
 import ToggleSwitch from "@/components/common/ToggleSwitch.vue";
+import router from "@/router";
 import { useAccountStore } from "@/stores/accountStore";
 import { Icon } from "@iconify/vue";
 import { onMounted, ref, computed } from "vue";
@@ -10,9 +11,6 @@ const accountStore = useAccountStore();
 const showGroupAccount = ref(false);
 
 const filteredAccountList = computed(() => {
-  console.log("현재 토글 상태:", showGroupAccount.value);
-  console.log("전체 계좌:", accountStore.accountList);
-
   if (showGroupAccount.value) {
     return accountStore.accountList.filter((account) => account.type === "group");
   } else {
@@ -29,7 +27,14 @@ onMounted(() => {
   <div class="h-full">
     <div class="flex justify-between items-center mb-3">
       <ToggleSwitch :label="'모임통장 보기'" @click="showGroupAccount = !showGroupAccount" />
-      <div class="flex">
+      <div
+        class="flex"
+        @click="
+          showGroupAccount
+            ? router.push({ name: 'group-account-create' })
+            : router.push({ name: 'import-personal-accounts' })
+        "
+      >
         <p class="caption3 text-gray-500">계좌 추가하기</p>
         <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
       </div>

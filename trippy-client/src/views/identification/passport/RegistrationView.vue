0ce4a87cd69f34ea522aef3fe7ff1f5bf49f94ed
bangbang444 <kim1228com@naver.com>
2025-07-28 15:28:48 +0900

    feat: 여권 등록 뷰

---
+++ trippy-client/src/views/identification/passport/RegistrationView.vue
@@ -0,0 +1,67 @@
+<script setup>
+import Idcard from "@/assets/Idcard.png";
+import ActionButtons from "@/components/identification/ActionButtons.vue";
+import DocumentPreview from "@/components/identification/CapturePreview.vue";
+import CountrySelect from "@/components/identification/CountrySelect.vue";
+import DatePicker from "@/components/identification/DatePicker.vue";
+import EditableField from "@/components/identification/EditableField.vue";
+import GenderSelect from "@/components/identification/GenderSelect.vue";
+import { ref } from "vue";
+
+const name = ref("홍길동");
+const englishName = ref("HONG/GILDONG");
+
+const editingField = ref({
+  name: false,
+  englishName: false,
+  id: false,
+  date: false,
+  nationality: false,
+  passportId: false,
+});
+const nationality = ref("KOR"); // 기본값 (대한민국)
+const passportId = ref("M12345678");
+</script>
+
+<template>
+  <div class="w-full flex flex-col">
+    <DocumentPreview :image="Idcard" message="여권 정보를 확인해주세요." />
+
+    <div class="mt-5">
+      <EditableField
+        label="이름"
+        v-model="name"
+        :readonly="!editingField.name"
+        @toggleEdit="editingField.name = !editingField.name"
+      />
+
+      <EditableField
+        label="영문이름"
+        v-model="englishName"
+        :readonly="!editingField.englishName"
+        @toggleEdit="editingField.englishName = !editingField.englishName"
+      />
+    </div>
+
+    <div class="flex gap-4">
+      <DatePicker class="flex-1" label="생년월일" v-model="birthDate" />
+      <GenderSelect class="flex-1" label="성별" v-model="gender" />
+    </div>
+
+    <div>
+      <CountrySelect :label="국적" v-model="nationality" />
+    </div>
+
+    <EditableField
+      label="여권번호"
+      v-model="passportId"
+      :readonly="!editingField.passportId"
+      @toggleEdit="editingField.passportId = !editingField.englishName"
+    />
+    <div>
+      <DatePicker class="flex-1" label="여권만료날짜" v-model="birthDate" />
+    </div>
+
+    <ActionButtons @leftClick="console.log('다시 촬영')" @rightClick="console.log('등록')" />
+  </div>
+</template>

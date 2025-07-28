69992f376c61280313af436207336e44e1a604b3
kang-yesung <rkddptjd5228@gmail.com>
2025-07-28 09:32:43 +0900

    feat 토글버튼 추가

--- trippy-client/src/views/AccountListview.vue
+++ trippy-client/src/views/AccountListview.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+import ToggleSwitch from "@/components/common/ToggleSwitch.vue";
+</script>
 
 <template>
-  <div>hi</div>
+  <div>
+    <div class="flex justify-between">
+      <ToggleSwitch />
+      <div>계좌 추가하기</div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>

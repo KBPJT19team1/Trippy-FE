d57699105c88ce24c81fa8d13312e1702a5d22f2
kang-yesung <rkddptjd5228@gmail.com>
2025-07-28 15:57:29 +0900

    feat 계좌 목록 뷰  퍼블리싱 완료

---
+++ trippy-client/src/components/account/CreateAccountButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <div class="bg-main-gradient w-[4rem] h-[4rem] rounded-[100%] flex items-center justify-center">
+    <Icon icon="material-symbols:add-2-rounded" class="size-10 text-white" />
+  </div>
+</template>
+
+<style scoped></style>
--- trippy-client/src/views/AccountListview.vue
+++ trippy-client/src/views/AccountListview.vue
@@ -5,20 +5,18 @@ import router from "@/router";
 import AccountIcon from "@/assets/svg/account-icon.svg";
 import { useAccountStore } from "@/stores/accountStore";
 import { Icon } from "@iconify/vue";
-import { onMounted, ref, watch, nextTick } from "vue";
+import { onMounted, ref, watch, nextTick, computed } from "vue";
+import CreateAccountButton from "@/components/account/CreateAccountButton.vue";
 
 const accountStore = useAccountStore();
-
 const showGroupAccount = ref(false);
-
 const scrollContainer = ref(null);
 
-const accountList = ref([]);
+const accountList = computed(() => accountStore.filterAccountList);
 
 onMounted(async () => {
   await accountStore.GetAccountList();
   accountStore.FilterAccount(showGroupAccount.value);
-  accountList.value = accountStore.filterAccountList;
 });
 
 // 토글 변화에 따라 계좌 목록 상단으로 이동 + 필터 적용
@@ -27,13 +25,12 @@ watch(showGroupAccount, async () => {
   await nextTick();
   if (scrollContainer.value) {
     scrollContainer.value.scrollTop = 0;
-    accountList.value = accountStore.filterAccountList;
   }
 });
 </script>
 
 <template>
-  <div class="h-full overflow-hidden">
+  <div class="flex flex-col justify-between h-full">
     <div class="flex justify-between items-center mb-3">
       <ToggleSwitch :label="'모임통장 보기'" @click="showGroupAccount = !showGroupAccount" />
       <div
@@ -46,28 +43,42 @@ watch(showGroupAccount, async () => {
         "
       >
         <p class="caption3 text-gray-500">계좌 추가하기</p>
-        <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+        <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180 text-gray-500" />
+      </div>
+    </div>
+
+    <div class="flex flex-col flex-1 overflow-scroll [&::-webkit-scrollbar]:hidden">
+      <div
+        v-if="accountList.length === 0"
+        class="flex flex-col justify-center items-center h-full gap-3"
+      >
+        <AccountIcon />
+        <p class="text-gray-400 subtitle2">
+          {{ showGroupAccount ? "등록된 모임 계좌가 없습니다..." : "등록된 계좌가 없습니다..." }}
+        </p>
+      </div>
+
+      <div v-else ref="scrollContainer">
+        <UnifiedAccountCard
+          v-for="(account, i) in accountList"
+          :key="i"
+          :account="account"
+          :ifGroupAccount="showGroupAccount"
+          class="my-3"
+        />
       </div>
     </div>
 
     <div
+      class="flex justify-end mt-3"
       v-if="accountList.length === 0"
-      class="flex flex-col h-full justify-center items-center mt-[-5rem] gap-3"
+      @click="
+        showGroupAccount
+          ? router.push({ name: 'group-account-create' })
+          : router.push({ name: 'import-personal-accounts' })
+      "
     >
-      <AccountIcon />
-      <p class="text-gray-400 subtitle2">
-        {{ showGroupAccount ? "등록된 모임 계좌가 없습니다..." : "등록된 계좌가 없습니다..." }}
-      </p>
-    </div>
-
-    <div class="h-[38rem] overflow-scroll [&::-webkit-scrollbar]:hidden" ref="scrollContainer">
-      <UnifiedAccountCard
-        v-for="(account, i) in accountList"
-        :key="i"
-        :account="account"
-        :ifGroupAccount="showGroupAccount"
-        class="my-3"
-      />
+      <CreateAccountButton />
     </div>
   </div>
 </template>

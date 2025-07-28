a76f5caea1390c638179d122651de5a741122430
i-soj-ng <jeong4530@gmail.com>
2025-07-28 21:13:04 +0900

    feat : 채우기/보내기 버튼 퍼블리싱

--- trippy-client/src/views/personal-accounts/DetailView.vue
+++ trippy-client/src/views/personal-accounts/DetailView.vue
@@ -1,12 +1,18 @@
 <script setup>
-
+import TransferButton from "@/components/common/TransferButton.vue";
 </script>
 
 <template>
   <div class="w-full h-full">
-    <div class="bg-white">
-      <p class="body2">국민은행 123-1232312-123</p>
-      <h1 class="title1">23,456,789원</h1>
+    <div class="bg-white flex flex-col gap-4">
+      <div class="flex flex-col gap-2">
+        <p class="body2">국민은행 123-1232312-123</p>
+        <h1 class="title1">23,456,789원</h1>
+      </div>
+      <div class="flex gap-4">
+        <TransferButton type="add" />
+        <TransferButton type="send" />
+      </div>
     </div>
   </div>
 </template>

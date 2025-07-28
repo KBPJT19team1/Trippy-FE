961488e145e9a10b1f362dd0fef06847b659fb1b
juwon1105 <amy1105a@naver.com>
2025-07-28 15:37:04 +0900

    fix : 카드설정뷰&메인결제뷰에 카드가 없을 경우 추가

--- trippy-client/src/components/payment/CardCarousel.vue
+++ trippy-client/src/components/payment/CardCarousel.vue
@@ -1,29 +1,19 @@
 <script setup>
+defineProps({
+  cards: Array,
+});
+
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
 
-import card1 from "@/assets/card1.png";
-import card2 from "@/assets/card2.png";
-import card3 from "@/assets/card3.png";
-import plusCard from "@/assets/empty_card.png"; // + 카드
-
-const router = useRouter();
-
-const cards = [
-  { id: 1, image: card1, name: "KB국민카드_트래블러스" },
-  { id: 2, image: card2, name: "신한카드_디스커버" },
-  { id: 3, image: card3, name: "하나카드_위시올" },
-  { id: 999, image: plusCard, name: "카드 추가", isAddCard: true },
-];
-
 const activeIndex = ref(0);
+const router = useRouter();
 
 function onSlideChange(swiper) {
   activeIndex.value = swiper.realIndex;
 }
-
 function goToAddCard() {
   router.push("/payment/add");
 }

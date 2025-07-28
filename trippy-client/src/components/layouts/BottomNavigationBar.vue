c9f69c1c50df971e1d3dd35a54523d08d9f7ee46
Jaejeong Lee <leejae938@gmail.com>
2025-07-28 16:12:47 +0900

    [fix] git pull 충돌 후 해결

--- trippy-client/src/components/layouts/BottomNavigationBar.vue
+++ trippy-client/src/components/layouts/BottomNavigationBar.vue
@@ -6,9 +6,6 @@ import PayIcon from "@/assets/svg/payment-icon.svg";
 import LogIcon from "@/assets/svg/log-icon.svg";
 
 const route = useRoute();
-<<<<<<< HEAD
-const hiddenPrefixes = ["/group-account", "/personal-accounts", "/check/tickets", "/exchange"];
-=======
 const hiddenPrefixes = [
   "/group-account",
   "/personal-accounts",
@@ -16,19 +13,14 @@ const hiddenPrefixes = [
   "/check/bouchers",
   "/exchange",
 ];
->>>>>>> aedca5949075d4365693e38f464552508653cd36
 const isHidden = computed(() => hiddenPrefixes.some((prefix) => route.path.startsWith(prefix)));
 </script>
 
 <template>
-<<<<<<< HEAD
-  <div v-if="!isHidden" class="bg-white h-[90px] w-full fixed bottom-0 flex justify-center md:max-w-[375px] md:mx-auto">
-=======
   <div
     v-if="!isHidden"
     class="bg-white h-[90px] w-full fixed bottom-0 flex justify-center md:max-w-[375px] md:mx-auto"
   >
->>>>>>> aedca5949075d4365693e38f464552508653cd36
     <div class="h-[56px] mb-[34px] flex align-center items-center gap-14">
       <RouterLink to="/">
         <div class="text-gray-400 flex flex-col items-center hover:text-gray-600">

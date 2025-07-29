b3b88db10531c220104304da926c935f5e733481
kang-yesung <rkddptjd5228@gmail.com>
2025-07-29 15:39:11 +0900

    feat 모임계좌 설정 뷰 사용자 권한에 따라 분기처리

--- trippy-client/src/views/group-account/settings/GroupAccountSettingsWrapper.vue
+++ trippy-client/src/views/group-account/settings/GroupAccountSettingsWrapper.vue
@@ -1,14 +1,36 @@
 <script setup>
+import { onMounted, ref } from "vue";
 import MemberSettings from "@/components/group-account/MemberSettings.vue";
 import OwnerSettings from "@/components/group-account/OwnerSettings.vue";
-import { ref } from "vue";
 
-const isOwner = ref(true);
+const account = ref({});
+const member = ref([]);
+
+const isOwner = ref(false);
+
+onMounted(() => {
+  const currentUserId = 1;
+  const ownerId = 1;
+  isOwner.value = currentUserId === ownerId;
+
+  (account.value = {
+    accountName: "트리피",
+    accountBank: "국민은행",
+    accountNumber: "013456878547486",
+    createDate: "2025.07.15",
+  }),
+    (member.value = [
+      { id: 1, name: "홍길동" },
+      { id: 2, name: "김철수" },
+    ]);
+});
 </script>
 
 <template>
-  <OwnerSettings v-if="isOwner" />
-  <MemberSettings v-else />
+  <div class="h-full w-full">
+    <OwnerSettings v-if="isOwner" :account="account" :member="member" />
+    <MemberSettings v-else :account="account" :member="member" />
+  </div>
 </template>
 
 <style scoped></style>

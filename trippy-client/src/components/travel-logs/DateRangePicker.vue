2a66cc8633bffe4a6ffafad5ae16a68ec8df01b3
hsw <hsw8245@gmail.com>
2025-07-29 15:19:30 +0900

    feat : 달력 박스 크기 조정

--- trippy-client/src/components/travel-logs/DateRangePicker.vue
+++ trippy-client/src/components/travel-logs/DateRangePicker.vue
@@ -1,4 +1,10 @@
-<!-- DateRangePicker.vue -->
+<script setup>
+import { ref } from "vue";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+const dateRange = ref([]);
+</script>
+
 <template>
   <VueDatePicker
     v-model="dateRange"
@@ -6,13 +12,13 @@
     format="yyyy.MM.dd"
     :enable-time-picker="false"
     @update:model-value="$emit('update:range', { start: dateRange[0], end: dateRange[1] })"
+    input-class-name="custom-datepicker-input"
   />
 </template>
 
-<script setup>
-import { ref } from "vue";
-import VueDatePicker from "@vuepic/vue-datepicker";
-import "@vuepic/vue-datepicker/dist/main.css";
-
-const dateRange = ref([]);
-</script>
+<style scoped>
+/* DatePicker의 input 필드에 스타일 적용 */
+:deep(.dp__input) {
+  @apply w-full border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400;
+}
+</style>

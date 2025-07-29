8ee4dbbc6b3bb0f65967cb1d14ddee2f364b11ad
i-soj-ng <jeong4530@gmail.com>
2025-07-29 17:09:53 +0900

    feat : 로그인 뷰 BottomNavigationBar 숨김 처리

--- trippy-client/src/components/layouts/BottomNavigationBar.vue
+++ trippy-client/src/components/layouts/BottomNavigationBar.vue
@@ -22,6 +22,7 @@ const hiddenPrefixes = [
   "/group-settle",
   "/identification",
   "/payment/add",
+  "/login",
 ];
 
 const isHidden = computed(() => hiddenPrefixes.some((prefix) => route.path.startsWith(prefix)));

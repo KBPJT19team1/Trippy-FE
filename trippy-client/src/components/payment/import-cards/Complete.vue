7d95a91fe20c207446c0098c11558b57211cb1a1
juwon1105 <amy1105a@naver.com>
2025-07-29 17:37:59 +0900

    fix : 로고 크기 동일하게 설정

--- trippy-client/src/components/payment/import-cards/Complete.vue
+++ trippy-client/src/components/payment/import-cards/Complete.vue
@@ -10,14 +10,17 @@ const handleClick = () => {
 </script>
 
 <template>
-  <div class="flex flex-col h-full items-center justify-center">
-    <div class="w-full flex flex-col items-center gap-4">
-      <TrippyLogo class="w-40" />
-      <h3 class="title4">카드 불러오기가 완료되었습니다.</h3>
+  <div class="flex flex-col h-full justify-between">
+    <!-- 상단 문구 및 로고 -->
+    <div class="flex-1 flex items-center justify-center">
+      <div class="flex flex-col items-center gap-4">
+        <TrippyLogo class="w-64 h-36" />
+        <h3 class="title4">카드 불러오기가 완료되었습니다.</h3>
+      </div>
     </div>
-    <div
-      class="fixed bottom-0 left-0 right-0 z-50 w-full max-w-full pt-4 pb-[34px] px-4 bg-white md:max-w-[375px] md:mx-auto"
-    >
+
+    <!-- 하단 버튼 -->
+    <div class="w-full pt-4 pb-[34px] px-4 bg-white md:max-w-[375px] md:mx-auto">
       <NextButton title="확인" @click="handleClick" />
     </div>
   </div>

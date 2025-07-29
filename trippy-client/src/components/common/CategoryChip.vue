9cfbe54d89344d9628f4345ff4f31c0aca9d800c
i-soj-ng <jeong4530@gmail.com>
2025-07-29 09:39:36 +0900

    design : 카테고리칩 디자인 수정

--- trippy-client/src/components/common/CategoryChip.vue
+++ trippy-client/src/components/common/CategoryChip.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 
 <template>
   <div
-    class="flex items-center justify-center w-fit px-4 h-6 rounded-full"
+    class="flex items-center justify-center w-14 h-7 rounded-full"
     :class="[props.isSelect ? 'bg-blue-200 text-blue-400' : 'bg-gray-200 text-gray-500']"
   >
     <span class="caption2">{{ props.title }}</span>
--- trippy-client/src/views/personal-accounts/AccountDetailView.vue
+++ trippy-client/src/views/personal-accounts/AccountDetailView.vue
@@ -16,16 +16,31 @@ import CategoryChip from "@/components/common/CategoryChip.vue";
         <TransferButton type="send" />
       </div>
     </div>
-    <div class="bg-gray-100 h-6 mx-[-16px]"></div>
-    <div class="py-4">
-      <div class="flex gap-2 items-center text-gray-500">
-        <p class="body1">전체</p>
-        <Icon icon="material-symbols:arrow-forward-ios-rounded" class="w-4 h-4 rotate-90" />
+    <div class="bg-gray-100 h-4 mx-[-16px]"></div>
+    <div class="flex flex-col gap-4 py-4">
+      <div class="flex gap-1 items-center text-gray-500">
+        <p class="body2">전체</p>
+        <Icon icon="material-symbols:arrow-forward-ios-rounded" class="size-3 rotate-90" />
       </div>
-      <div>
-        <CategoryChip title="식비" :isSelect="true" />
-        <CategoryChip title="교통비" :isSelect="false" />
+      <!-- 거래내역 리스트 -->
+      <!-- Todo: 거래내역 카테고리 칩 위치 고민 -->
+      <div class="flex flex-col gap-2">
+        <p class="body2 text-gray-500">7월 28일</p>
+        <div class="flex justify-between">
+          <div class="flex gap-2 items-center">
+            <CategoryChip title="식비" :isSelect="true" />
+            <div class="flex flex-col gap-1">
+              <p class="subtitle1">현금 출금</p>
+              <p class="body2 text-gray-500">14:06</p>
+            </div>
+          </div>
+          <div class="flex flex-col gap-1 items-end">
+            <p class="subtitle1">-100,000원</p>
+            <p class="body2 text-gray-500">254,200원</p>
+          </div>
+        </div>
       </div>
+
     </div>
   </div>
 </template>

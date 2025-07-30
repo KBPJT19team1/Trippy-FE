34b3123083a9a02a1f9472030c59c9f1f68bff7f
i-soj-ng <jeong4530@gmail.com>
2025-07-30 10:27:44 +0900

    feat: 채우기 버튼 클릭시 모달 열기

---
+++ trippy-client/src/components/account/SelectAccountModal.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  isModalOpen: String,
+});
+</script>
+
+<template>
+  <div v-if="props.isModalOpen">
+    <h3>modal</h3>
+  </div>
+</template>

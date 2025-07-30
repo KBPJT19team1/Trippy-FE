14d686770e3cd033f3690ef2bfc79ae64e94c933
i-soj-ng <jeong4530@gmail.com>
2025-07-30 17:21:22 +0900

    feat : 송금 완료 뷰 퍼블리싱

---
+++ trippy-client/src/components/account/CompleteTransfer.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useRouter } from "vue-router";
+import TrippyLogo from "@/assets/svg/trippy-logo.svg";
+import NextButton from "@/components/common/NextButton.vue";
+import { numberWithCommas } from "@/assets/utils/index.js";
+
+const router = useRouter();
+</script>
+
+<template>
+  <div class="flex flex-col w-full h-full justify-between">
+    <div class="flex flex-col grow gap-2 title2 items-center justify-center mb-4">
+      <TrippyLogo class="w-40 mb-8" />
+      <h3>내 국민은행 계좌로</h3>
+      <h3>{{ `${numberWithCommas(100000)}원을` }}</h3>
+      <h3>보냈어요</h3>
+    </div>
+    <NextButton :title="'완료'" @click="router.push('/')" />
+  </div>
+</template>
+
+<style scoped></style>
--- trippy-client/src/views/personal-accounts/AddMoneyView.vue
+++ trippy-client/src/views/personal-accounts/AddMoneyView.vue
@@ -4,8 +4,9 @@ import { ref, computed } from "vue";
 import AmountInputView from "@/views/AmountInputView.vue";
 import PasswordInput from "@/components/common/inputs/PasswordInput.vue";
 import ConfirmTransfer from "@/components/account/ConfirmTransfer.vue";
+import CompleteTransfer from "@/components/account/CompleteTransfer.vue";
 
-const views =  [AmountInputView, PasswordInput, ConfirmTransfer];
+const views =  [AmountInputView, PasswordInput, ConfirmTransfer, CompleteTransfer];
 const currentIndex = ref(2);
 
 const currentView = computed(() => views[currentIndex.value]);

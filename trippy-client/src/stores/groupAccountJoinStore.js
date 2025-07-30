e99e73c54531bd951f43b53bf443b4d1274a6a4d
kang-yesung <rkddptjd5228@gmail.com>
2025-07-30 13:05:09 +0900

    feat store 변수 추가 및 출력

--- trippy-client/src/stores/groupAccountJoinStore.js
+++ trippy-client/src/stores/groupAccountJoinStore.js
@@ -11,8 +11,10 @@ export const useGroupJoinStore = defineStore("groupJoin", () => {
   const representativeAccount = ref("");
   //대표계좌 은행
   const representativeAccountBank = ref("");
-  // 모임원 분류
+  //모임원 분류
   const userRole = ref("member");
+  //가입 시간
+  const joinDateTime = ref("2025.07.29 13:13:13");
 
   const setRepresentativeAccount = (number, bank) => {
     representativeAccount.value = number;
@@ -26,6 +28,7 @@ export const useGroupJoinStore = defineStore("groupJoin", () => {
     representativeAccountBank,
     userRole,
     groupAccountName,
+    joinDateTime,
     setRepresentativeAccount,
   };
 });

d3ead64a4d4825968389fa5db3765967371d4a41
kang-yesung <rkddptjd5228@gmail.com>
2025-07-30 10:23:44 +0900

    feat 모임계좌 참여 동의 뷰 완성

--- trippy-client/src/views/group-account/join/AgreeTermsView.vue
+++ trippy-client/src/views/group-account/join/AgreeTermsView.vue
@@ -1,7 +1,35 @@
-<script setup></script>
+<script setup>
+import TrippyLogo from "@/assets/svg/trippy-logo.svg";
+import AgreementCheck from "@/components/common/AgreementCheck.vue";
+import NextButton from "@/components/common/NextButton.vue";
+import { ref } from "vue";
+
+const checked = ref(false);
+</script>
 
 <template>
-  <div>hi</div>
+  <div class="felx felx-col justify-between h-full w-full">
+    <div class="h-full w-full flex flex-col items-center">
+      <div class="subtitle1 text-center">
+        <p>모임통장 사용을 위해</p>
+        <p>서비스 설명과 약관을 확인해 주세요.</p>
+      </div>
+      <TrippyLogo class="w-60 h-auto mt-32" />
+      <div class="mt-32 px-1">
+        <AgreementCheck
+          :title="'모임통장 설명서 및 약관에 동의'"
+          :class="checked ? 'bg-main-gradient' : 'bg-gray-400'"
+          @click="checked = !checked"
+        />
+        <p class="caption2 px-2 text-gray-400 mt-3">
+          모임통장은 모임주 개인 명의의 통장으로 납입한 모임 회비의지급, 해지 권한은 모임주에게
+          있습니다. 압류 등 모임주의 상태에 따라 모임 회비 및 모임통장 서비스 이용이 제한 될 수
+          있습니다.
+        </p>
+      </div>
+    </div>
+    <NextButton :title="'다음'" :disabled="!checked" />
+  </div>
 </template>
 
 <style scoped></style>

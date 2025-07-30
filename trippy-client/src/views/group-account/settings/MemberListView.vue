20c1d770f7e0e09ce60f9037f26197e2dc3982ef
kang-yesung <rkddptjd5228@gmail.com>
2025-07-30 21:02:55 +0900

    feat 모임원 보기 뷰 퍼블리싱 완료

--- trippy-client/src/views/group-account/settings/MemberListView.vue
+++ trippy-client/src/views/group-account/settings/MemberListView.vue
@@ -1,27 +1,49 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import SearchedMember from "@/components/common/inputs/SearchedMember.vue";
 import { useGroupMemberStore } from "@/stores/groupMemberStore";
 
 const memberStore = useGroupMemberStore();
-const leaderMember = ref("");
-const member = ref([]);
+const leaderMember = ref({});
+const members = ref([]);
 const searchedMember = ref("");
 
-onMounted;
+const filteredMembers = computed(() => {
+  if (searchedMember.value.trim() == "") {
+    return [];
+  }
+  return memberStore.groupMembers.filter(
+    (member) => member.name && member.name.includes(searchedMember.value),
+  );
+});
+
+onMounted(async () => {
+  await memberStore.setGroupMember();
+  leaderMember.value = memberStore.groupMembers.find((m) => m.role === "leader");
+  members.value = memberStore.groupMembers.filter((m) => m.role === "member");
+});
 </script>
 
 <template>
-  <SearchedMember v-model="searchedMember" />
+  <div class="h-full w-full overflow-auto">
+    <SearchedMember v-model="searchedMember" />
 
-  <div>
-    <p>모임주</p>
-    <p>{{ leaderMember }}신형만</p>
-  </div>
+    <div v-if="!searchedMember" class="px-2 mt-5">
+      <div class="mb-4">
+        <p class="caption1 text-gray-500 mb-1">모임주</p>
+        <p class="body2">{{ leaderMember.name }}</p>
+      </div>
+      <div>
+        <p class="caption1 text-gray-500 mb-1">멤버</p>
+        <p class="body2 mb-1" v-for="member in members" :key="member.id">{{ member.name }}</p>
+      </div>
+    </div>
 
-  <div>
-    <p>멤버</p>
-    <p>{{}}</p>
+    <div v-else class="px-2 mt-5">
+      <p class="body2 mb-1" v-for="member in filteredMembers" :key="member.id">
+        {{ member.name }}
+      </p>
+    </div>
   </div>
 </template>
 

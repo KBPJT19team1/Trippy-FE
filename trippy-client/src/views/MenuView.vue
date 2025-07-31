75c23dfc31af128a03948b0a114b53d35b27d202
kang-yesung <rkddptjd5228@gmail.com>
2025-07-31 19:39:43 +0900

    feat 메뉴 페이지 기능별 router처리

--- trippy-client/src/views/MenuView.vue
+++ trippy-client/src/views/MenuView.vue
@@ -1,7 +1,90 @@
-<script setup></script>
+<script setup>
+import router from "@/router";
+import { Icon } from "@iconify/vue";
+</script>
 
 <template>
   <main>
-    <h1>menu</h1>
+    <div class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2">
+      <p class="">마이페이지</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2">
+      <p class="">설정</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'ExchangeCurrency' })"
+    >
+      <p class="">환전하기</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'payment' })"
+    >
+      <p class="">결제</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'account-list' })"
+    >
+      <p class="">계좌목록</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'payment' })"
+    >
+      <p class="">카드목록</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'identification' })"
+    >
+      <p class="">신분증</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'AirTicket' })"
+    >
+      <p class="">항공권</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'bouchers' })"
+    >
+      <p class="">바우처</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push({ name: 'travel-logs' })"
+    >
+      <p class="">여행로그</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
+
+    <div
+      class="flex justify-between caption1 py-4 border-b border-b-gray-300 px-2"
+      @click="router.push('/translation')"
+    >
+      <p class="">번역기</p>
+      <Icon icon="material-symbols:arrow-back-ios-new-rounded" class="rotate-180" />
+    </div>
   </main>
 </template>

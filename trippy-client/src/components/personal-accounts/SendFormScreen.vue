6ea2be9ac0d2cd39114070345f163b99946d58c0
i-soj-ng <jeong4530@gmail.com>
2025-07-31 13:38:29 +0900

    feat : 계좌 입력뷰 Input 창 연결

--- trippy-client/src/components/personal-accounts/SendFormScreen.vue
+++ trippy-client/src/components/personal-accounts/SendFormScreen.vue
@@ -1,7 +1,15 @@
 <script setup>
+import { ref } from "vue";
+import AmountInput from "@/components/common/inputs/AmountInput.vue";
 
+const account = ref("");
 </script>
 
 <template>
-
+  <div class="flex flex-col w-full h-full">
+    <div class="flex flex-col gap-8">
+      <h2 class="title2">어떤 계좌로 보낼까요?</h2>
+      <AmountInput v-model="account" type="account" />
+    </div>
+  </div>
 </template>

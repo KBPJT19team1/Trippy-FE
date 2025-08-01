0dc8a9d053e1f89594ca3e984c804103130b3ac8
kang-yesung <rkddptjd5228@gmail.com>
2025-08-01 17:35:21 +0900

    feat 컴포넌트 적용

--- trippy-client/src/views/group-account/transfer/TransferCompleteView.vue
+++ trippy-client/src/views/group-account/transfer/TransferCompleteView.vue
@@ -1,24 +1,9 @@
 <script setup>
-import NextButton from "@/components/common/NextButton.vue";
-import { numberWithCommas } from "@/assets/utils/index.js";
-import TrippyLogo from "@/assets/svg/trippy-logo.svg";
-import router from "@/router";
-
-const onClick = () => {
-  router.push({ name: "home" });
-};
+import CompleteTransfer from "@/components/account/CompleteTransfer.vue";
 </script>
 
 <template>
-  <div class="flex flex-col w-full h-full justify-between">
-    <div class="flex flex-col grow gap-8 title2 items-center justify-center mb-4">
-      <TrippyLogo class="w-40" />
-      <div class="flex flex-col gap-2 items-center">
-        <h3>모임원들의 계좌로</h3>
-        <h3>{{ `${numberWithCommas(100000)}원을` }}</h3>
-        <h3>보냈어요</h3>
-      </div>
-    </div>
-    <NextButton title="완료" :disabled="false" @click="onClick" />
+  <div class="h-full">
+    <CompleteTransfer />
   </div>
 </template>
